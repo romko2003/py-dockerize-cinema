03344309dbf7adc7fe0c9bb9655dd0e3c22c5a3d
= <=>
2025-09-20 00:32:38 +0300

    implement

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_spectacular.views import (SpectacularAPIView,
+                                   SpectacularSwaggerView)
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,16 +12,22 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
+    path("api/schema/", SpectacularAPIView.as_view(),
+         ame="schema"),
+    path("api/docs/",
+         SpectacularSwaggerView.as_view(url_name="schema"),
+         name="docs"),
 
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/", TokenObtainPairView.as_view(),
+         name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(),
+         name="token_refresh"),
 
     path("api/", include("cinema.urls")),
 ]
 
 if settings.DEBUG:
-    # Для dev: віддавати і media, і static (щоб Swagger/DRF UI працювали під gunicorn)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
